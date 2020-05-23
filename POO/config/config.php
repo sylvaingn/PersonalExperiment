de2bd1fd86de3aa35dd0f699171e3bf94e3a5bfe
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-05-23 11:54:03 +0200

    déclaration autoloader composer

--- POO/config/config.php
+++ POO/config/config.php
@@ -1,17 +1,2 @@
 <?php
 
-const FOLDERS = [
-    'controllers',
-    'models'
-];
-
-spl_autoload_register(function ($class) {
-
-    foreach(FOLDERS as $folder) {
-        $file =  __DIR__ . '/../src/'. $folder .'/' . $class . '.php';
-
-        if (file_exists($file)) {
-                include $file;
-        }
-    }
-});
