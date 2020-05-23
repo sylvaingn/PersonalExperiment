8a5d8b79ba3d312e7cb5ce7680218ef958e22367
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-05-23 16:59:59 +0200

    ajout acces asset

--- POO/src/controller/AbstractController.php
+++ POO/src/controller/AbstractController.php
@@ -5,13 +5,16 @@ namespace App\Controller;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-abstract class AbstractController {
+abstract class AbstractController
+{
 
-    public static function getTwig() {
+    public static function getTwig()
+    {
         $loader = new FilesystemLoader(__DIR__ . '/../../views');
         $twig = new Environment($loader);
+        $twig->addGlobal('assets',  BASE_PATH . '/assets');
+        $twig->addGlobal('base_path',  BASE_PATH);
 
         return $twig;
     }
-
 }
