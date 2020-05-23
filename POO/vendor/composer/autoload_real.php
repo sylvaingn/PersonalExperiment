91ef5f9db887d8aea8dba716bd9124664aa336e2
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-05-23 11:31:07 +0200

    install symfony/var_dumper

--- POO/vendor/composer/autoload_real.php
+++ POO/vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInit5f2838bd3f083d93119ab983474556c8
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit5f2838bd3f083d93119ab983474556c8::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire5f2838bd3f083d93119ab983474556c8($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire5f2838bd3f083d93119ab983474556c8($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
