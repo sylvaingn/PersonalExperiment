22ef2528d770985e0385f7dcd1c6806a3570b107
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-05-23 16:55:23 +0200

    creation appcontroller

---
+++ POO/src/controller/AppController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+class AppController extends AbstractController
+{
+
+    public static function index()
+    {
+        echo self::getTwig()->render('app/index.html');
+    }
+}
